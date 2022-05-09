18b4bf6c51b7bcf6d5560bbcbd1ab286ec5cc162
Galina Pugovkina <Galinaplus@bk.ru>
2022-05-09 15:27:53 +0400

    task final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,21 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {order: {}, goods: []};
 
     let countOfGoods = goods.length;
+    data.client = name + ' ' + phone;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    data.order.address = 'ул. ' + address.street + ', дом ' + address.house + ', ' + address.entrance + ' подъезд, ' + address.floor + ' этаж, кв ' + address.flat;
+    data.order.sum = sum;
+
+    for (let i = 0; i <countOfGoods; i += 1) {
+        let rezaltGoods = {title:'',count:0};
+        rezaltGoods.title = goods[i].title;
+        rezaltGoods.count = goods[i].count;
+        data.goods.push(rezaltGoods);        
     }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
